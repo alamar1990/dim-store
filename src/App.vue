60d93e33f3448e98018484f64a49f0f5d0236722
Alain Marquez <alamar1990@gmail.com>
2022-11-08 11:34:13 -0500

    Switch to scss preprocessor

--- src/App.vue
+++ src/App.vue
@@ -30,16 +30,19 @@ import HelloWorld from './components/HelloWorld.vue'
   <HelloWorld msg="Vite + Vue" />
 </template>
 
-<style scoped>
+<style lang="scss" scoped>
 .logo {
   height: 6em;
   padding: 1.5em;
   will-change: filter;
+&:hover {
+   filter: drop-shadow(0 0 2em #646cffaa);
+ }
 }
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
+.logo.vue {
+&:hover {
+   filter: drop-shadow(0 0 2em #42b883aa);
+ }
 }
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+
 </style>
